beaf3fb93d09bbbe968ae7d4077906bbd0897a22
Jason Papakostas <vithos@gmail.com>
2022-02-16 11:22:19 +0100

    generate user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'sqlite',
       database: 'db.sqlite',
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
